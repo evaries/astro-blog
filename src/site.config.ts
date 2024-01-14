a0edb581c47bb0d53337951a89593024be61ec96
evaries <nadtochii.e@gmail.com>
2024-01-14 11:19:21 +0800

    adjusted seo

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,7 +1,9 @@
+import type { Langs } from "./i18n/ui";
+
 interface SiteConfig {
 	author: string;
-	title: string;
-	description: string;
+	title: Record<Langs, string>;
+	description: Record<Langs, string>;
 	lang: string;
 	ogLocale: string;
 	themeColorLight: string;
@@ -16,9 +18,15 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "Yevhenii Nadtochii",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Yevhenii Nadtochii",
+	title: {
+		en: "Yevhenii Nadtochii - Affordable Websites for Small Businesses | Unbeatable Designs, User-Friendly, and Stunning Visual Appeal",
+		uk: "Доступні веб-сайти для бізнесів | Неперевершені дизайни, зручні в користуванні та неймовірно естетичні",
+	},
 	// Meta property used as a default description meta property
-	description: "Personal blog of Yevhenii Nadtochii",
+	description: {
+		en: "Elevate your small to medium-sized business with our affordable websites. Experience unbeatable designs, user-friendly interfaces, and stunning visual appeal that will resonate with your customers. Your online success begins here",
+		uk: "Прокачайте свій малий чи середній бізнес за допомогою доступних веб-сайтів. Оцініть неперевершені дизайни, зручні інтерфейси та вражаючі візуальні ефекти, які сподобаються вашим клієнтам. Ваш успіх в Інтернеті починається тут.",
+	},
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
